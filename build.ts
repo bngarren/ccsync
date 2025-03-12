b8079a2f9aba123635b9e8fdb7bef5a52ba512ba
Ben Garren <bngarren@gmail.com>
2025-03-12 14:08:34 -0400

    fix(build): added bun-plugin-pino to help bundle pino's dependencies

--- build.ts
+++ build.ts
@@ -1,8 +1,15 @@
+import { bunPluginPino } from "bun-plugin-pino"
+
 try {
   const result = await Bun.build({
     entrypoints: ["./src/index.ts"],
     outdir: "./dist",
     target: "node",
+    plugins: [
+      bunPluginPino({
+        transports: ["pino-roll"],
+      }),
+    ],
   })
   if (result.success) {
     console.log("Build success.", result.outputs[0].path)
